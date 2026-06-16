b84ca2e1c6bf1fc6a75f4c35cd896b5d811f0959
kundan gupta <kundankumar712712712@gmail.com>
2026-06-16 11:24:34 +0530

    Backend: Added Prisma ORM

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,9 @@
 import express from "express";
+import dotenv from "dotenv";
 
-
+dotenv.config();
 const app = express();
 
-app.listen(8080, ()=>{
-    console,log("Server is running on port 8080");
+app.listen(process.env.PORT,()=>{
+    console.log("Server is running on port 8080");
 })
