eab532e24cdb0287c01547cdb613da81a467b725
kundan gupta <kundankumar712712712@gmail.com>
2026-06-19 15:34:27 +0530

    feat: implement problem creation with Judge0 validation

--- backend/src/controllers/problem.controller.js
+++ backend/src/controllers/problem.controller.js
@@ -1,4 +1,4 @@
-import {db} from "../libs/db.js";
+import { db } from "../libs/db.js";
 import { poolBatchResults, submitBatch } from "../libs/judge0.lib.js";
 import { getJudeg0LanguageId } from "../libs/judge0.lib.js";
 
@@ -15,28 +15,58 @@ export const createProblem = async (req, res) => {
 
     try {
 
-        for (const [language, solutionCode] of Object.entries(referenceSolution)){
+        for (const [language, solutionCode] of Object.entries(referenceSolution)) {
 
             const languageId = getJudeg0LanguageId(language);
 
-            if(!languageId){
+            if (!languageId) {
                 return res.status(400).json({
-                    error:`Language ${language} is not supported`
+                    error: `Language ${language} is not supported`
                 })
             }
 
-            const submissions= testCases.map(({input, output}) => ({
+            const submissions = testCases.map(({ input, output }) => ({
                 language_id: languageId,
                 source_code: solutionCode,
                 stdin: input,
-                expected_output:output
+                expected_output: output
             }))
 
             const submissionResult = await submitBatch(submissions);
             const tokens = submissionResult.map((res) => res.token);
             const results = await poolBatchResults(tokens);
 
-            
+            for (let i = 0; i < results.length; i++) {
+
+                const result = results[i];
+                if (result.status.id !== 3) {
+                    return res.status(400).json({
+                        error: `Testcase ${i + 1} failed for language ${language}`
+                    })
+                }
+            }
+
+            // save the problem to the database
+            const newProblem = await db.problem.create({
+                data: {
+                    title,
+                    description,
+                    difficulty,
+                    tags,
+                    example,
+                    constraints,
+                    testCases,
+                    codeSnippets,
+                    referenceSolution,
+                    userId: req.user.id,
+                }
+            });
+
+            return res.status(201).json({
+                newProblem, 
+                message: "Problem created successfully"
+            });
+
         }
     }
     catch (err) {
